a7b4cd44692310232eace980507b356ab412e2bf
Damodar Lohani <dlohani48@gmail.com>
2022-03-31 11:15:14 +0000

    basic functionality

---
+++ functions/stripe_payment/lib/stripe_payment.dart
@@ -0,0 +1,18 @@
+import 'package:stripedart/stripedart.dart';
+
+Future<void> start(final request, final response) async {
+  var stripe = Stripe(request.env['STRIPE_SECRET_KEY']);
+
+  final customer = await stripe.core.customers
+      .create(params: {"email": request.env['email']});
+  print(customer);
+  final paymentIntent = await stripe.core.paymentIntents!.create(params: {
+    "amount": 100,
+    "currency": "USD",
+    "customer": customer!['id'],
+  });
+  print(paymentIntent);
+  response.json({
+    "client_secret": paymentIntent!['client_secret'],
+  });
+}
